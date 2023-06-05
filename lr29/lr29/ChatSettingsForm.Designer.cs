00eb0c1f26f9c5c37fe4eb8759ed57b5678178a0
Yana Kapustian <65828184+YanaKapustian@users.noreply.github.com>
2023-06-05 20:18:54 +0300

    OOP 29 laba

---
+++ lr29/lr29/ChatSettingsForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace lr29
+{
+    partial class ChatSettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatLogPathTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chatLogPathTextBox
+            // 
+            this.chatLogPathTextBox.Location = new System.Drawing.Point(371, 192);
+            this.chatLogPathTextBox.Name = "chatLogPathTextBox";
+            this.chatLogPathTextBox.Size = new System.Drawing.Size(347, 22);
+            this.chatLogPathTextBox.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 75);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Зберегти до шляху";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 75);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вивести лог";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ChatSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chatLogPathTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "ChatSettingsForm";
+            this.Text = "ChatSettingsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox chatLogPathTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
